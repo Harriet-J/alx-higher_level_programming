3afae7f3ea623670fad34c565581f65fb3bb7b4f
Harriet-J <seremharriet@gmail.com>
2024-09-11 02:50:24 -0700

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 
-print(''.join('{:c}'.format(letter) for letter in range(97, 123) if chr(letter) not in 'qe'), end='')
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
